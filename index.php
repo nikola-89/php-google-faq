c4b1c34f0861194e7ea5b0dc8b0abcde3d2c6e70
Nicola <nicola.ba1989@gmail.com>
2020-02-24 15:49:20 +0100

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" href="//ssl.gstatic.com/policies/favicon.ico" sizes="32x32">
+        <link rel="stylesheet" href="css/master.css">
+        <title>Domande frequenti – Privacy e termini – Google</title>
+    </head>
+    <body>
+        <header>
+            <div class="navbar">
+                <div class="top">
+                    <img src="https://i.imgur.com/82LYOWE.png" alt="logo">
+                    <a href="#">Privacy e termini</a>
+                </div>
+                <div class="list">
+                    <ul>
+                        <li><a href="#">Introduzione</a></li>
+                        <li><a href="#">Norme sulla privacy</a></li>
+                        <li><a href="#">Termini di servizio</a></li>
+                        <li><a href="#">Tecnologie</a></li>
+                        <li><a href="#">Domande frequenti</a></li>
+                        <li><a href="#">Account Google</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
